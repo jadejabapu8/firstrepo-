dfe5ae29827e6a272251232683637097c0b9d8cb
jadejabapu8 <jadejamandipsinh300@gmail.com>
2025-05-20 13:55:12 +0530

    commit

---
+++ 16-5-2025/p3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    int a;
+   
+    
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    
+    
+    
+    
+    printf("you have enterd : %d",a);
+    
+    
+}
