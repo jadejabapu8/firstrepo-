1ff3c04b7d2a557b770a6580f9adf7b7e3949422
jadejabapu8 <jadejamandipsinh300@gmail.com>
2025-05-20 10:28:38 +0530

    commit

---
+++ p2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main()
+{
+    int a = 5;
+    int A = 6;
+    float b = 56.56;
+    char c = 'n';
+    double d = 26.18255;
+    
+    
+    
+    printf("\n value is : %d",a);
+    printf("\n value is : %d",A);
+    printf("\n value is : %f",b);
+    printf("\n value is : %c",c);
+    printf("\n value is : %lf",d);
+    
+    getch();
+}
