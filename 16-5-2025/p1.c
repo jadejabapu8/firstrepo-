e7d2414a29338e005cffbf6b7b8c0fc8d4d535a7
jadejabapu8 <jadejamandipsinh300@gmail.com>
2025-06-02 13:48:53 +0530

    commit

---
+++ 16-5-2025/p1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    printf ("\n hello sir jadeja ");
+    printf (" \t hello mr jadeja ");
+    printf ("\n hello mandip sinh ");
+}
---
+++ 16-5-2025/p3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+    int a= 26;
+    float b= 26.18;
+    char c= 'n';
+    double d= 26.1826;
+    
+    
+    
+    printf ("\n value is : %d",a);
+    printf ("\n value is : %f",b);
+    printf ("\n value is : %c",c);
+    printf ("\n value is : %lf",d);
+    
+    getch ();
+}
